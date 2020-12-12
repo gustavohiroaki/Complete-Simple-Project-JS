bf25995b15ba6db542d58e5c013fd58edc6cd90e
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2020-12-12 11:42:54 +0000

    feat(project): added error returns

--- backend/src/interfaces/controller/ProjectController.js
+++ backend/src/interfaces/controller/ProjectController.js
@@ -21,12 +21,16 @@ class ProjectController {
     const { id } = req.params;
     const project = DeleteProject(id, ProjectInMemoryAccess);
 
+    if (project.error) return res.status(400).json(project);
+
     return res.status(200).json(project);
   }
   update(req, res) {
     const { id } = req.params;
     const { title, owner } = req.body;
     const project = UpdateProject(id, title, owner, ProjectInMemoryAccess);
+
+    if (project.error) return res.status(400).json(project);
 
     return res.status(200).json(project);
   }
